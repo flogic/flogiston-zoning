feed5715d3935eeb3e3fae7c0f2b6e339d95cab7
Yossef Mendelssohn <ymendel@pobox.com>
2011-10-23 21:46:51 -0400

    Associating Site to Domain.
    And providing `Site.for_domain` for retrieving the site for a given
    domain name.

--- app/models/flogiston/site.rb
+++ app/models/flogiston/site.rb
@@ -1,6 +1,13 @@
 class Flogiston::Site < ActiveRecord::Base
+  has_many :domains
+
   validates_presence_of   :name
   validates_presence_of   :handle
   validates_uniqueness_of :name
   validates_uniqueness_of :handle
+
+  def self.for_domain(name)
+    return nil unless domain = Domain.find_by_name(name)
+    domain.site
+  end
 end
